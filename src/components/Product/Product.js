bf21cf9c7f0cd9f54c0fd0bf8c9d6e9cf8d9561f
Injamul Haque Rashik <ihrashik@gmail.com>
2020-08-28 06:54:43 +0600

    Product Portion Done!

---
+++ src/components/Product/Product.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "../Product/Product.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+const Product = (props) => {
+  return (
+    <div className="product">
+      <div>
+        <img src={props.product.img} alt="" />
+      </div>
+      <div className="product-info">
+        <h5 className="product-name">{props.product.name}</h5>
+        <br />
+        <p>
+          <small>By : {props.product.seller}</small>
+        </p>
+        <br />
+        <p>
+          <b>Price : ${props.product.price}</b>
+        </p>
+        <br />
+        <p>
+          <small>
+            Only <b>{props.product.stock}</b> pieces left in stock - Order Soon
+          </small>
+        </p>{" "}
+        <br />
+        <button className="add-button">
+          {" "}
+          <FontAwesomeIcon icon={faShoppingCart} />
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
