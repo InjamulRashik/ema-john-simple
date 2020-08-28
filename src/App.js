3fad617d61f36d7b9856d014af5084716a015f2e
Injamul Haque Rashik <ihrashik@gmail.com>
2020-08-28 19:59:31 +0600

    Netlify Deployment Done!

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Shop from "./components/Shop/Shop";
-import Product from "./components/Product/Product";
+//import Product from "./components/Product/Product";
 
 function App() {
   return (
